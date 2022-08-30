deda1b0758a3bb685af84ab688eec330f5355fc3
GurgelDavi <davigurgel2@gmail.com>
2022-08-30 18:13:26 -0300

    onde parei os estudos antes de trocar máquina

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json())
+
+const livros = [
+    {id:1,"titulo":"Senhor dos Aneis"},
+    {id:2,"titulo":"O Hobbit"}
+]
+
+app.get('/',(req,res) => {
+    res.status(200).send('Curso de node')
+})
+
+app.get('/livros',(req,res) => {
+    res.status(200).json(livros)
+})
+
+app.post('/livros',(req,res) => {
+    livros.push(req.body)
+    res.status(201).send('livro adicionado')
+})
+
+export default app
